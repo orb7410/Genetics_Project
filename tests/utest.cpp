1a970b3af91342d034680ea6a3136ae8a5338bba
orb7410 <orb7410@gmail.com>
2023-07-30 16:28:39 +0300

    1 test

--- tests/utest.cpp
+++ tests/utest.cpp
@@ -4,15 +4,45 @@
 #include <vector> // std::vector
 #include <cstdint> // uint32_t
 
+#include "get_patient_data.hpp"
+#include "update_patient_data.hpp"
+#include "iget_patient_data.hpp"
+#include "iupdate_patient_data.hpp"
+#include "patient.hpp"
+#include "common_types.hpp"
 
-BEGIN_TEST(search_query)
-	using namespace se;
-	SearchEngine s;
-	s.execute("../utils/configuration.json");
-	ASSERT_PASS();
+std::string name = "Or";
+std::string id = "23862577";
+bool gender = false;
+std::string gene = "ABT";
+std::string varient = "c<52.0"
+
+BEGIN_TEST(Update_get_data)
+
+	IUpdatePatientData Iupdate;
+	UpdatePatientData update(Iupdate);
+
+	IGetPatientData IgetD;
+	GetPatientData getD(Iget);
+
+	update.updateName(name);
+    update.updateId(id);
+    update.updateGender(gender);
+    update.updateGene(gene, varient);
+	
+	ASSERT_EQUAL(getD.getName(), name);
+	ASSERT_EQUAL(getD.getId(), id);
+	ASSERT_EQUAL(getD.getGender(), GENDER::MALE);
+END_TEST
+
+BEGIN_TEST(get_data_empty)
+//empty var name too
+END_TEST
+
+BEGIN_TEST(update_many_varient_same_gene)
 END_TEST
 
 
-TEST_SUITE(因果応報 [m1 search engine])
-	TEST(search_query)
+TEST_SUITE(因果応報 [m1 Genetic project])
+	TEST(Update_get_data)
 END_SUITE

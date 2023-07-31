4807bd0b4eb3b8585d35202460bd5cf65faff869
adayaTovim <adaya012@gamail.com>
2023-07-31 12:44:08 +0300

    create process data class - just skeleton

---
+++ inc/process_data.hpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <sstream>
+
+#include "data_base.hpp"
+
+namespace genetics {
+
+class ProcessData {
+public:
+	void generateSummaryLetter(const DataBase& a_database);
+
+};
+
+} // namespace genetics

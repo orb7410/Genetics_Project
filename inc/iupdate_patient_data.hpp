eeac1d5f7d2daa5c89a10c3194e17abdffec7c28
h73.or <orb7410@gmail.com>
2023-07-31 12:48:43 +0300

    corect files

--- inc/iupdate_patient_data.hpp
+++ inc/iupdate_patient_data.hpp
@@ -7,16 +7,18 @@
 
 #include "common_types.hpp"
 
-namespace Genetics {
+namespace Genetics
+{
 
-class IUpdatePatientData {
-public:
-    virtual ~IUpdatePatientData() = default;
+    class IUpdatePatientData
+    {
+    public:
+        virtual ~IUpdatePatientData() = default;
 
-    virtual void updateName(std::string const& a_name) = 0;
-    virtual void updateId(std::string const& a_id) = 0;
-    virtual void updateGender(bool a_gender) = 0;
-    virtual void updateGene(std::string const& a_gene, std::string const& a_varient) = 0;
-};
+        virtual void updateName(std::string const &a_name) = 0;
+        virtual void updateId(std::string const &a_id) = 0;
+        virtual void updateGender(bool a_gender) = 0;
+        virtual void updateGene(std::string const &a_gene, std::string const &a_varient) = 0;
+    };
 
 } // se
